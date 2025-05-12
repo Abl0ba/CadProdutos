8917f8a53086bb53d2f10471efa1e0ef47ce3456
Eli-ge <lgfplayer1@gmail.com>
2025-05-11 22:50:48 -0400

    Update exibir.php

--- cadprodutos/controller/exibir.php
+++ cadprodutos/controller/exibir.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-  <meta charset="UTF-8">
-  <title>Produtos</title>
-  <link rel="stylesheet" href="view/assets/css/estilo.css">
+	<meta charset="UTF-8">
+ 	<title>Produtos</title>
+	<link rel="stylesheet" href="view/assets/css/estilo.css">
 </head>
 <body>
-  <h1 class="titulo">Produtos</h1>
-  <p id="apresentacao">Aqui você pode cadastrar um produto ou visualizar um produto cadastrado</p>
- </body>
+	<h1 class="titulo">Produtos</h1>
+	<p id="apresentacao">Aqui você pode cadastrar um produto ou visualizar um produto cadastrado</p>
+</body>
+ <br>
+<body>
+	<p id="cadastro" href="cadprodutos/controller/cadastro.php">Cadastre o produto aqui</p>
 </html>
