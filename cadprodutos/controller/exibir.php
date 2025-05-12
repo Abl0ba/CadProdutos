a4e38c2b4a65091c3104eff05a3e9d0e95f6572f
Eli-ge <lgfplayer1@gmail.com>
2025-05-11 22:41:25 -0400

    Update exibir.php

---
+++ cadprodutos/controller/exibir.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Produtos</title>
+  <link rel="stylesheet" href="view/assets/css/estilo.css">
+</head>
+<body>
+  <h1 class="titulo">Cadastro de produtos</h1>
+  <p id="apresentacao">Cadastre o produto preenchendo as seguintes categorias:</p>
+ </body>
+</html>
