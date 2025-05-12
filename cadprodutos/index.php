bd379e8d0aa30437ff3b1b669c73805f63474310
Eli-ge <lgfplayer1@gmail.com>
2025-05-11 22:40:56 -0400

    Update index.php

--- cadprodutos/index.php
+++ cadprodutos/index.php
@@ -1,18 +1,4 @@
 <?php
-// Ponto de entrada do projeto
-// header("Location: controller/exibir.php");
-// exit;
+header("Location: controller/exibir.php");
+exit;
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Exemplo de CSS</title>
-    <link rel="stylesheet" href="view/assets/css/estilo.css">
-</head>
-<body>
-    <h1 class="titulo">Bem-vindos ao Estudo de CSS</h1>
-    <p id="apresentacao">Hoje vamos aprender o básico de CSS.</p>
-</body>
-</html>
