5460d7abaf59819a7c72f1fa53fa4819f51a48c7
Eli-ge <lgfplayer1@gmail.com>
2025-05-11 23:04:14 -0400

    Update cadastro.php

--- cadprodutos/controller/cadastro.php
+++ cadprodutos/controller/cadastro.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
 
+<head>
+	<meta chraset="UTF-8">
+	<title>Cadastro de produtos</title>
+</head>
+
+<body>
+	<h1 class="titulocad">Cadastre o produto</h1>
+	<p id="apresentacaocad">Preencha os campos para cadastrar o produto</p>
+ <br>
+  <label for="nomeprod">Nome do produto:</label><br>
+  <input type="text" id="nomeprod" name="nomeprod"><br>
+  <input type="submit">
+</body>
+</html>
